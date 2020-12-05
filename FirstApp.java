26586403701b518f2e482de3288318fddc93000a
Radoslaw Choluj <radekcholuj@wp.pl>
2020-12-05 21:59:35 +0100

    utworzenie klasy i maina

---
+++ FirstApp.java
@@ -0,0 +1,29 @@
+class FirstApp {
+	public static void main (String args[]) {
+		// piewrszy program
+		System.out.println("Dzsiaj jest piêkna pogoda");
+
+		System.out.println ();
+
+		// zadanie 1
+		System.out.println("Pan pomidor wlaz³ na tyczkê");
+		System.out.println("I przedrzeŸnia ogrodniczkê.");
+		System.out.println("\"Jak pan mo¿e,");
+		System.out.println("Panie pomidorze?!\"\n");
+
+
+		// zadanie 2
+		System.out.println ("----------------------");
+		System.out.println ("Do biedronki przyszed³ ¿uk,");
+		System.out.println ("W okieneczko puk-puk-puk.\n");
+		System.out.println ("Panieneczka widzi ¿uka:");	
+		System.out.println ("\"Czego Pan tu u mnie szuka?\"");
+		System.out.println ("----------------------\n");
+
+		//zadanie 3
+		System.out.println ("*******************************");
+		System.out.println ("*\"To, ¿e milczê, nie znaczy,  *");
+		System.out.println ("*¿e nie mam nic do powiedzenia*.\"");
+		System.out.println ("*******************************");
+	}
+}
